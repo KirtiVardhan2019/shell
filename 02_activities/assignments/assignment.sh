87bad16d5e0298a758bf12fd11a088cf26819c76
Kirti <kvardhan@mun.ca>
2025-08-03 13:52:27 -0400

    assigment completed

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -24,11 +24,31 @@ touch analysis/main.py
 curl -Lo rawdata.zip https://github.com/UofT-DSI/shell/raw/refs/heads/main/02_activities/assignments/rawdata.zip
 unzip -q rawdata.zip
 
+mkdir data
+#cd ./newproject
+mv ./rawdata ./data
+mv ./data/rawdata ./data/raw
+ls ./data/raw
+mkdir ./data/procesed
+mkdir ./data/procesed/server_logs ./data/procesed/user_logs ./data/procesed/event_logs
+
+mv ./data/raw/server* ./data/procesed/server_logs/
+mv ./data/raw/event* ./data/procesed/event_logs/
+mv ./data/raw/user* ./data/procesed/user_logs/
+
+rm -r ./data/raw/ipaddr*
+rm -r ./data/procesed/user_logs/user_ipaddr*
+
+ls ./data/procesed/event_logs/ > ./data/inventory.txt
+ls ./data/procesed/server_logs/ >> ./data/inventory.txt
+ls ./data/procesed/user_logs/ >> ./data/inventory.txt
+
+
 ###########################################
 # Complete assignment here
 
 # 1. Create a directory named data
-mkdir data
+
 # 2. Move the ./rawdata directory to ./data/raw
 
 # 3. List the contents of the ./data/raw directory
